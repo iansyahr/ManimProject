616275e2a0fb1fe4cb7a21b3596ef79bc549464a
Muhamad Apriansyah Ramadhan <37446681+iansyahr@users.noreply.github.com>
2022-08-02 13:38:44 +0700

    Update Amplitudo.py

--- Gelombang/Amplitudo.py
+++ Gelombang/Amplitudo.py
@@ -3,7 +3,7 @@ from manim import *
 class Amplitudo(Scene):
     def construct(self):
         ax = Axes(
-            x_range=[0, 2*PI], y_range=[-1.5, 1.5], axis_config={"include_tip": True}
+            x_range=[0, 2*PI], y_range=[-1.5, 1.5], axis_config={"include_tip": True, "stroke_width":10}
         )
         labels = ax.get_axis_labels(x_label="x", y_label="y")
 
@@ -20,7 +20,7 @@ class Amplitudo(Scene):
         plane = NumberPlane()
 
         self.add(plane)
-        self.play(AnimationGroup(Create(ax),Create(graph),lag_ratio = 0))
+        self.play(AnimationGroup(Create(ax),Create(graph),lag_ratio = 0), run_time = 0.5)
 
         arrow = Arrow([-3,0,0], [-3,2,0], buff=0)
         arrow1 = DashedLine([-3,0,0], [-3,2,0],stroke_width = 10, dash_length=0.1).add_tip()
@@ -29,5 +29,14 @@ class Amplitudo(Scene):
         text = MathTex("A",font_size=70)
         text.shift([-3,2.6,0])
 
-        self.play(AnimationGroup(GrowFromPoint(arrow1,[-3,0,0]),GrowFromCenter(bulat),Write(text),lag_ratio = 0))
+        AmplitudoTex = Text("Amplitudo")
+        AmplitudoTex.shift(RIGHT*3 + UP*2)
+        kotakuning = Rectangle(color = YELLOW, height = 3.8, width=6.6)
+        kotakuning.shift(DOWN*2)
+
+        amplitudoall = VGroup(arrow1,bulat,text,ax,graph,AmplitudoTex)
+
+        self.play(AnimationGroup(GrowFromPoint(arrow1,[-3,0,0]),GrowFromCenter(bulat),Write(text),Write(AmplitudoTex),lag_ratio = 0, run_time = 0.5))
         self.wait()
+        self.play(AnimationGroup(amplitudoall.animate.scale(0.5).shift(DOWN*2), Create(kotakuning), lag_ratio = 0))
+        self.wait(2)
