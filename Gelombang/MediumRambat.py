88eb0f05eee9a76dd17e693932a4d728d95d262b
Muhamad Apriansyah Ramadhan <37446681+iansyahr@users.noreply.github.com>
2022-08-02 13:42:53 +0700

    Update MediumRambat.py

--- Gelombang/MediumRambat.py
+++ Gelombang/MediumRambat.py
@@ -2,8 +2,16 @@ from manim import *
 
 class MediumRambat(Scene):
     def construct(self):
-      kotak = SVGMobject("kotak.svg",height = 5, stroke_color = WHITE,)
-      gelombang = SVGMobject("gelombangdesmos.svg",height = 1, stroke_color = WHITE)
-      self.play(Create(kotak))
-      self.play(Create(gelombang))
+      kotak = SVGMobject("/content/ManimProject/Gelombang/kotak.svg",height = 5,stroke_color = WHITE).set_stroke(width = 8)
+      gelombang = SVGMobject("/content/ManimProject/Gelombang/gelombangdesmos.svg",height = 1, stroke_color = WHITE).set_stroke(width = 8)
+      terbang = NumberPlane()
+      kotakkuning = Rectangle(color = YELLOW, height = 3.8, width=6.6)
+      kotakkuning.shift(LEFT*3.5 + UP*2)
+      MediumRambatTex = Text("Medium Rambat", font_size = 35)
+      MediumRambatTex.shift(RIGHT*4 + UP*2)
+
+      MediumRambatAll = VGroup(kotak,gelombang,MediumRambatTex)
+      self.add(terbang)
+      self.play(AnimationGroup(Create(kotak),Create(gelombang),Write(MediumRambatTex), lag_ratio = 0.1))
+      self.play(AnimationGroup(MediumRambatAll.animate(lag_ratio = 0, run_time = 1).scale(0.5).shift(LEFT*4.5 + UP*2),Create(kotakkuning), lag_ratio=0))
       self.wait(5)
